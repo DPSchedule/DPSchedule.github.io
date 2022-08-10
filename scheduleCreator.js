a9b1f5d6626a749771f776e1c556ae9abbc64ff7
SameerDalal <sameerdalal747@gmail.com>
2022-08-10 10:51:30 -0400

    adding randome lab generator

--- scheduleCreator.js
+++ scheduleCreator.js
@@ -2,7 +2,7 @@
 
 let labList = ["EML","Thinker Space","EMM","EMF","Kid Science","Scipod Level 2","Scipod Level 1","Scipod Level 3"]
 function generateSchedules(fhdv,shdv,fdv){
-    createScoreboardTable(tableHeaders = ["Team Member","9:15-10:00","10:00-11:00","11:00-11:30","11:30-12:00","12:00-12:30","12:30-1:00","1:00-1:30","1:30-2:00","2:00-3:00","3:00-4:00","4:00-4:30"])
+    createScoreboardTable(["Team Member","9:15-10:00","10:00-11:00","11:00-11:30","11:30-12:00","12:00-12:30","12:30-1:00","1:00-1:30","1:30-2:00","2:00-3:00","3:00-4:00","4:00-4:30"])
     
 
 }
